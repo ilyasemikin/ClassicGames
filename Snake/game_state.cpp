8295ef69703d103ca6df23057f1c49a4941a0c4b
ilyasemikin <iasemikin@gmail.com>
2020-01-07 17:24:59 +0300

    Snake: implemented snake

--- Snake/game_state.cpp
+++ Snake/game_state.cpp
@@ -9,7 +9,7 @@ GameState::~GameState() {
 
 }
 
-Rectangle GameState::getScoreRect() {
+Rectangle GameState::getTopRect() {
 	Rectangle ret;
 	auto wndSize { _data->window.getSize() };
 	ret.x = 0;
@@ -29,38 +29,153 @@ Rectangle GameState::getMainRect() {
 	return ret;
 }
 
-float GameState::getBlockSize(const Rectangle &rect) {
+float GameState::getBlockSize() {
 	auto widthRatio { 
-		static_cast<float>(_field.width) / rect.width 
+		static_cast<float>(_field.width) / _output.mainRect.width 
 	};
 	auto heightRation {
-		static_cast<float>(_field.height) / rect.height
+		static_cast<float>(_field.height) / _output.mainRect.height
 	};
 	if (widthRatio < heightRation)
-		return static_cast<float>(rect.height) / _field.height;
+		return static_cast<float>(_output.mainRect.height) / _field.height;
 	else
-		return static_cast<float>(rect.width) / _field.width;
+		return static_cast<float>(_output.mainRect.width) / _field.width;
+}
+
+bool GameState::isGameOver() {
+	auto newHead { getNewHead() };
+
+	if (newHead.x == 0 || newHead.x == _field.width - 1 ||
+	    newHead.y == 0 || newHead.y == _field.height - 1)
+		return true;
+
+	for (auto &point : _snake.points)
+		if (point.x == newHead.x && point.y == newHead.y)
+			return true;
+
+	return false;
+}
+
+sf::Vector2u GameState::getNewHead() {
+	const std::map<Direction, sf::Vector2i> offsets {
+		{ Direction::DOWN, { 0, 1 } },
+		{ Direction::LEFT, { -1, 0 } },
+		{ Direction::RIGHT, { 1, 0 } },
+		{ Direction::UP, { 0, -1 } }
+	};
+
+	auto offset { offsets.at(_snake.direction) };
+	return sf::Vector2u(_snake.head.x + offset.x, _snake.head.y + offset.y); 
+}
+
+void GameState::changeDirection(Direction newDirection) {
+	switch (newDirection) {
+		case Direction::DOWN:
+		case Direction::UP:
+			if (_snake.direction == Direction::LEFT || 
+			    _snake.direction == Direction::RIGHT)
+				_snake.direction = newDirection;
+			break;
+		case Direction::LEFT:
+		case Direction::RIGHT:
+			if (_snake.direction == Direction::UP ||
+			    _snake.direction == Direction::DOWN)
+				_snake.direction = newDirection;
+			break;
+	}
+}
+
+void GameState::makeStep() {
+	if (isGameOver())
+		return;
+
+	auto newHead { getNewHead() };
+	_snake.points.push_front(_snake.head);
+	_snake.points.pop_back();
+	_snake.head = newHead;
+}
+
+void GameState::updateDeltaTime() {
+	_deltaTime.time = _deltaTime.clock.getElapsedTime().asSeconds();
+}
+
+void GameState::drawScore() {
+	auto str { "Score: " + std::to_string(_scoreValue) };
+	_score.setString(str);
+	_data->window.draw(_score);
+}
+
+void GameState::drawField() {
+	auto isBoundaryCell {
+		[&](size_t x, size_t y) {
+			return x == 0 || x == _field.width - 1 || y == 0 || y == _field.height - 1;
+		}
+	};
+	sf::RectangleShape rect;
+	for (size_t i = 0; i < _field.width; i++)
+		for (size_t j = 0; j < _field.height; j++) {
+			if (isBoundaryCell(i, j))
+				rect = _fieldBoundary;
+			else if ((i + j) % 2 != 0)
+				rect = _oddEmptyCell;
+			else
+				rect = _evenEmptyCell;
+			
+			rect.setPosition(
+				_output.mainRect.x + i * _output.blockSize,
+				_output.mainRect.y + j * _output.blockSize
+			);
+			_data->window.draw(rect);
+		}
+}
+
+void GameState::drawSnake() {
+	sf::RectangleShape rect;
+	rect.setFillColor(sf::Color::Blue);
+	rect.setSize(sf::Vector2f(_output.blockSize, _output.blockSize));
+	
+	rect.setPosition(
+		sf::Vector2f(
+			_output.mainRect.x + _snake.head.x * _output.blockSize,
+			_output.mainRect.y + _snake.head.y * _output.blockSize
+		)
+	);
+	_data->window.draw(rect);
+	
+	for (auto &point : _snake.points) {
+		auto x { _output.mainRect.x + point.x * _output.blockSize };
+		auto y { _output.mainRect.y + point.y * _output.blockSize };
+		rect.setPosition(sf::Vector2f(x, y));
+		_data->window.draw(rect);
+	}
 }
 
 void GameState::init() {
+	_scoreValue = 0;
+	_output.topRect = getTopRect();
+	_output.mainRect = getMainRect();
+	_output.blockSize = getBlockSize();
+
+	for (size_t i = 1; i < 8; i++)
+		_snake.points.push_front(sf::Vector2u(i, _field.height / 2));
+	_snake.head = sf::Vector2u(8, _field.height / 2);
+	_snake.direction = Direction::RIGHT;
+
 	_data->assets.loadFont("RetroVilleNC", "Fonts/RetroVilleNC-Regular.ttf");
 
 	_score.setFont(_data->assets.getFont("RetroVilleNC"));
 	_score.setFillColor(sf::Color::White);
 	_score.setString("Score:");
-	auto scoreSpaceRect { getScoreRect() };
-	_score.setCharacterSize(scoreSpaceRect.height * 0.95);
+	_score.setCharacterSize(_output.topRect.height * 0.95);
 
-	auto mainSpaceRect { getMainRect() };
-	auto blockSize( getBlockSize(mainSpaceRect) );
 	_fieldBoundary.setFillColor(sf::Color(0, 64, 0));
-	_fieldBoundary.setSize(sf::Vector2f(blockSize, blockSize));
+	_fieldBoundary.setSize(sf::Vector2f(_output.blockSize, _output.blockSize));
 
 	_oddEmptyCell.setFillColor(sf::Color(0, 128, 0));
-	_oddEmptyCell.setSize(sf::Vector2f(blockSize, blockSize));
+	_oddEmptyCell.setSize(sf::Vector2f(_output.blockSize, _output.blockSize));
 
 	_evenEmptyCell.setFillColor(sf::Color(0, 192, 0));
-	_evenEmptyCell.setSize(sf::Vector2f(blockSize, blockSize));
+	_evenEmptyCell.setSize(sf::Vector2f(_output.blockSize, _output.blockSize));
 }
 
 void GameState::handleInput() {
@@ -71,6 +186,16 @@ void GameState::handleInput() {
 			case sf::Event::Closed:
 				_data->window.close();
 				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Up)
+					changeDirection(Direction::UP);
+				else if (event.key.code == sf::Keyboard::Down)
+					changeDirection(Direction::DOWN);
+				else if (event.key.code == sf::Keyboard::Left)
+					changeDirection(Direction::LEFT);
+				else if (event.key.code == sf::Keyboard::Right)
+					changeDirection(Direction::RIGHT);
+				break;
 			default:
 				break;
 		}
@@ -78,35 +203,20 @@ void GameState::handleInput() {
 }
 
 void GameState::update(float dt) {
+	updateDeltaTime();
 
+	if (_deltaTime.time > 0.1) {
+		makeStep();
+		_deltaTime.clock.restart();
+	}
 }
 
 void GameState::draw(float dt) {
 	_data->window.clear();
 
-	_data->window.draw(_score);
-	auto isBoundaryCell {
-		[&](size_t x, size_t y) {
-			return x == 0 || x == _field.width - 1 || y == 0 || y == _field.height - 1;
-		}
-	};
-	auto wndSize { getMainRect() };
-	auto blockSize { getBlockSize(wndSize) };
-	for (size_t i = 0; i < _field.width; i++)
-		for (size_t j = 0; j < _field.height; j++) {
-			sf::RectangleShape rect;
-			if (isBoundaryCell(i, j))
-				rect = _fieldBoundary;
-			else if ((i + j) % 2 != 0)
-				rect = _oddEmptyCell;
-			else
-				rect = _evenEmptyCell;
-			rect.setPosition(
-				wndSize.x + i * blockSize,
-				wndSize.y + j * blockSize
-			);
-			_data->window.draw(rect);
-		}
-			
+	drawScore();
+	drawField();
+	drawSnake();
+
 	_data->window.display();
 }
--- Snake/game_state.h
+++ Snake/game_state.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <list>
 #include <SFML/Graphics.hpp>
 #include "game_data.h"
 #include "rectangle.h"
@@ -15,20 +16,54 @@ private:
 	sf::RectangleShape _evenEmptyCell;
 
 	struct {
+		Rectangle topRect;
+		Rectangle mainRect;
+		float blockSize;
+	} _output;
+
+	struct {
 		size_t width;
 		size_t height;
 	} _field;
 
-	Rectangle getScoreRect();
+	struct {
+		sf::Clock clock;
+		float time;
+	} _deltaTime;
+
+	enum Direction { 
+		UP, DOWN, LEFT, RIGHT
+	};
+
+	struct {
+		sf::Vector2u head;
+		std::list<sf::Vector2u> points;
+		Direction direction;
+	} _snake;
+
+	size_t _scoreValue;
+
+	Rectangle getTopRect();
 	Rectangle getMainRect();
 
-	float getBlockSize(const Rectangle &rect);
+	float getBlockSize();
+
+	bool isGameOver();
+	sf::Vector2u getNewHead();
+	void changeDirection(Direction newDirection);
+	void makeStep();
+
+	void updateDeltaTime();
+
+	void drawScore();
+	void drawField();
+	void drawSnake();
 public:
 	GameState(GameDataRef data, size_t width, size_t height);
 	~GameState();
 
 	void init() override;
-	
+
 	void handleInput() override;
 	void update(float dt) override;
 	void draw(float dt) override;
